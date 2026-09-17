dd35fd1bf350305031ad2cbbfbc935f210941397
Prathap Induri <induriprathap03@gmail.com>
2026-09-17 23:30:18 +0530

    Fix CSRF trusted origin

--- StudentManagement/settings.py
+++ StudentManagement/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://studentmanagement-3hy0.onrender.com",
+]
 
 # Application definition
 
