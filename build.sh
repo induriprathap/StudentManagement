b157571da398c9f3ee11930108ce74aa024edb41
Prathap Induri <induriprathap03@gmail.com>
2026-09-18 11:24:38 +0530

    Seed default courses

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python manage.py shell -c "from students.models import Course; [Course.objects.get_or_create(name=name) for name in ['Python', 'Data Science', 'Machine Learning', 'Django']]"
